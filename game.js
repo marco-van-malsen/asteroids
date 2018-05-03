dd46cbb4df971fda2e704f6d2521adfc94f78900
van Malsen <vanmalse@splm.siemens.com>
2018-05-03 21:23:29 +0200

    more touch stuff

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-// Asteroids game
-// Original: Daniel Shiffman
-// Extended: Marco van Malsen
-
 // check if laser hit asteroids
 function checkLaserAsteroid() {
   // check all lasers
@@ -231,14 +227,6 @@ function newLevel() {
 
   // reset explosions
   explosions = [];
-}
-
-function touchStarted() {
-  if (gameState === GAME_PAUSED || gameState === GAME_NOT_STARTED) {
-    gamestate = GAME_STARTED;
-  } else if (gameState === GAME_STARTED) {
-    ship.heading = createVector(touches[0].x - ship.pos.x, touches[0].y - ship.pos.y);
-  }
 }
 
 // responsive UI
---
+++ touch.js
@@ -0,0 +1,11 @@
+// Asteroids game
+// Original: Daniel Shiffman
+// Extended: Marco van Malsen
+
+function touchStarted() {
+  if (gameState === GAME_PAUSED || gameState === GAME_NOT_STARTED) {
+    gamestate = GAME_STARTED;
+  } else if (gameState === GAME_STARTED) {
+    ship.heading = createVector(touches[0].x - ship.pos.x, touches[0].y - ship.pos.y);
+  }
+}
