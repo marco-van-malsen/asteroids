b73b46f2866a119a6dba37b42374b6fc8502c926
van Malsen <vanmalse@splm.siemens.com>
2018-05-03 21:01:25 +0200

    more touch support

--- game.js
+++ game.js
@@ -234,7 +234,11 @@ function newLevel() {
 }
 
 function touchStarted() {
-  ship.heading = createVector(touches[0].x - ship.pos.x, touches[0].y - ship.pos.y);
+  if (gameState === GAME_PAUSED || gameState === GAME_NOT_STARTED) {
+    gamestate = GAME_STARTED;
+  } else if (gameState === GAME_STARTED) {
+    ship.heading = createVector(touches[0].x - ship.pos.x, touches[0].y - ship.pos.y);
+  }
 }
 
 // responsive UI
